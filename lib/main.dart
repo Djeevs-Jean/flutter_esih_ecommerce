d278e0f544b78c0fb5484e2e37111835340626ab
Djeevs-Jean <jeanyvesbadette@gmail.com>
2023-05-03 23:01:35 -0400

    add myapp

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:bankhoo/utils/app_theme.dart';
-import 'package:bankhoo/data.dart';
 import 'package:bankhoo/pages/_details/payement_page.dart';
+import 'package:bankhoo/screen/home_page.dart';
 import 'package:bankhoo/auth/auth.dart';
-// import 'package:bankhoo/pages/_details/login_page.dart';
-import 'package:bankhoo/_widget/_widget_category.dart';
 import 'package:bankhoo/pages/_pages_drawer/drawer_page.dart';
-import 'package:bankhoo/models/article.dart';
-import 'package:bankhoo/_widget/_widget_article.dart';
 import 'package:bankhoo/pages/_pages_navigation/navigation_pages1.dart';
-import 'package:bankhoo/services/_product_services.dart';
-import 'package:bankhoo/services/_category_services.dart';
 
 void main() {
-  runApp(const MaterialApp(home: HomePageScreen()));
+  runApp(const MyApp());
 }
 
-class HomePageScreen extends StatefulWidget {
-  const HomePageScreen({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<HomePageScreen> createState() => _HomePageScreenState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: const MainScreen()
+    );
+  }
 }
 
-class _HomePageScreenState extends State<HomePageScreen> {
-  List<Widget> listWidget = [ FavoritePage(), BodyHomePageScreen(), CartPage()];
-  List<String> listWidgetTitle = ["EBootikoo", "Favorite Page", "Cart Page"];
-  int selectedIndex = 0;
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  List<Widget> listWidget = const[ FavoritePage(), HomePageScreen(), CartPage()];
+  List<String> listWidgetTitle = ["Favorite Page", "EBootikoo", "Cart Page"];
+  int selectedIndex = 1;
   
   @override
   Widget build(BuildContext context) {
@@ -43,17 +52,13 @@ class _HomePageScreenState extends State<HomePageScreen> {
               ],
             ),
           ),
-          
           ListTile(title: const Text("Connecter"), trailing: const Icon(Icons.login), onTap: (){
             Navigator.push(context, MaterialPageRoute(builder: (_) => const LoginPage()));
           },),
-
           ListTile(title: const Text("List Product"), trailing: const Icon(Icons.list_alt), onTap: (){
             Navigator.push(context, MaterialPageRoute(builder: (context) => const ProductListPage()));
           },),
-          
           ListTile(title: const Text("Deconnecter"), trailing: const Icon(Icons.logout), onTap: (){
-            // Navigator.push(context, MaterialPageRoute(builder: (_) => const LoginPage()));
           },),
         ],
       ),
@@ -66,7 +71,7 @@ class _HomePageScreenState extends State<HomePageScreen> {
           }, child: const Text("PEYE", style: AppTheme.titleHead,))
         ],
       ),
-    body:  listWidget.elementAt(selectedIndex),
+    body: listWidget.elementAt(selectedIndex),
     bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.favorite), label: "Favorite"),
@@ -84,72 +89,4 @@ class _HomePageScreenState extends State<HomePageScreen> {
   }
 }
 
-class BodyHomePageScreen extends StatefulWidget {
-  const BodyHomePageScreen({Key? key}) : super(key: key);
 
-  @override
-  State<BodyHomePageScreen> createState() => _BodyHomePageScreenState();
-}
-
-class _BodyHomePageScreenState extends State<BodyHomePageScreen> {
-
-  List<Article> _listArticles = [];
-  List<String> listCategories = [];
-  List listFavorites = DataApp.listFavorites;
-  List listCart = DataApp.listCart;
-  
-
-  Future<void> _fetchArticles() async {
-    final articles = await ArticleService.getArticles();
-    setState(() {
-      _listArticles = articles as List<Article>;
-    });
-  }
-
-  Future<void> _fetchCategory() async {
-    final categories = await CategoryService.getCategories();
-    print("load category");
-    setState(() {
-      listCategories = categories;
-    });
-  }
-  
-  @override
-  void initState() {
-    super.initState();
-    _fetchArticles();
-    _fetchCategory();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Container( padding: const EdgeInsets.only(top: 10), decoration: const BoxDecoration(),
-          child: Column(
-            children: [
-              Container(
-                alignment: Alignment.centerLeft,
-                margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 10,),
-                child: const Text("Categories", style: AppTheme.categoryTop),
-              ),
-              CategoryListWidget(listCategory: listCategories),
-            ],
-          ),
-        ),
-      
-        Container(
-          alignment: Alignment.centerLeft,
-          margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-          child: const Text("Articles", style: AppTheme.categoryTop),
-        ),
-
-        SingleChildScrollView(
-          child:ArticleListWidget(
-            listArticles: _listArticles, 
-            listCart: listCart, listFavorites: listFavorites),
-        ),
-      ],
-    );
-  }
-}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:bankhoo/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const HomePageScreen());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
