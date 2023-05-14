bfcc56397d3c66812f1b78cacce1822b2cebc0e2
Djeevs-Jean <jeanyvesbadette@gmail.com>
2023-05-14 17:00:12 -0400

    CartPage

--- lib/screen/cart_page.dart
+++ lib/screen/cart_page.dart
@@ -3,16 +3,10 @@ import 'package:flutter_ecommerce/notifier_chg.dart';
 import 'package:flutter_ecommerce/_widget/_widget_product.dart';
 import 'package:flutter_ecommerce/storage/storage.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_ecommerce/data_program.dart';
 
-class CartPage extends StatefulWidget {
+class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
-  @override
-  State<CartPage> createState() => _CartPageState();
-}
-
-class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
     MyAppStateNotifier state = context.watch<MyAppStateNotifier>();
@@ -25,28 +19,5 @@ class _CartPageState extends State<CartPage> {
         return Storage.getPanierProduct(-1);
       });
     }
-/*     List listCart = DataProgram.listCart;
-
-    return listCart.isEmpty ? const Center(child: Text("Tu n'as pas d'article dans le panier"),) :
-    ListView.builder(
-      itemCount: listCart.length,
-      itemBuilder: (BuildContext context, int index) {
-      return ListTile(
-        leading: Image.network(
-            listCart[index].image,
-            width: 50,
-            height: 50,
-          ),
-          title: Text(listCart[index].title),
-          subtitle: Text('\$${listCart[index].price}'),
-          trailing: IconButton(
-            icon: const Icon(Icons.add_shopping_cart),
-            onPressed: () {
-              // Ajouter le produit au panier ici
-            },
-          ),
-        );
-      },
-    );
- */  }
+  }
 }
