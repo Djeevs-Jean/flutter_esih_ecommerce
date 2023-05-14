5f204cd13730a7dc492479f2f3518ceffc2a6534
Djeevs-Jean <jeanyvesbadette@gmail.com>
2023-05-14 16:11:40 -0400

    productlist_page

--- lib/screen/drawer_page/productlist_page.dart
+++ lib/screen/drawer_page/productlist_page.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/service/api_service.dart';
-import 'package:flutter_ecommerce/data_program.dart';
-import 'package:flutter_ecommerce/models/article.dart';
-import 'package:flutter_ecommerce/services/_product_services.dart';
 import 'package:flutter_ecommerce/_widget/_widget_product.dart';
 
 class ProductListPage extends StatefulWidget {
@@ -14,32 +11,13 @@ class ProductListPage extends StatefulWidget {
 
 class _ProductListPageState extends State<ProductListPage> {
 
-  List<Article> _listArticles = [];
-  List listFavorites = DataProgram.listFavorites;
-  List listCart = DataProgram.listCart;
-
-  Future<void> _fetchArticles() async {
-    final articles = await ArticleService.getArticles();
-    setState(() {
-      _listArticles = articles as List<Article>;
-    });
-  }
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _fetchArticles();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Center(child: Text("List Produits"))),
-      body: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
-        child: const ProductListWidget(getProducts: APIService.getProducts),
-      ),
+      body: const SingleChildScrollView(
+        child: ProductListWidget(getProducts: APIService.getProducts),
+      ) 
     );
   }
 }
--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ecommerce/models/article.dart';
-import 'package:flutter_ecommerce/services/_product_services.dart';
 import 'package:flutter_ecommerce/service/api_service.dart';
-import 'package:flutter_ecommerce/services/_category_services.dart';
 import 'package:flutter_ecommerce/utils/app_theme.dart';
 import 'package:flutter_ecommerce/_widget/_widget_product.dart';
 import 'package:flutter_ecommerce/_widget/_widget_category.dart';
-import 'package:flutter_ecommerce/data_program.dart';
 
 class HomePageScreen extends StatefulWidget {
   const HomePageScreen({Key? key}) : super(key: key);
@@ -16,36 +12,6 @@ class HomePageScreen extends StatefulWidget {
 }
 
 class _HomePageScreenState extends State<HomePageScreen> {
-
-  List<Article> _listArticles = [];
-  List<String> listCategories = [];
-  List listFavorites = DataProgram.listFavorites;
-  List listCart = DataProgram.listCart;
-  
-
-  Future<void> _fetchArticles() async {
-    final articles = await APIService.getTopProducts();
-    // final articles = await ArticleService.getArticles();
-    setState(() {
-      _listArticles = articles as List<Article>;
-    });
-  }
-
-  Future<void> _fetchCategory() async {
-    final categories = await CategoryService.getCategories();
-
-    print("load category");
-    setState(() {
-      listCategories = categories;
-    });
-  }
-  
-  @override
-  void initState() {
-    super.initState();
-    _fetchArticles();
-    _fetchCategory();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -68,6 +34,5 @@ class _HomePageScreenState extends State<HomePageScreen> {
         ],
       )
     );
-    
   }
 }
