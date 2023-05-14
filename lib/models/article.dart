a9d2b9436ff8a2644fb5abd806e06fed39a1fe42
Djeevs-Jean <jeanyvesbadette@gmail.com>
2023-05-14 18:38:45 -0400

    ArticleModel

--- lib/models/article.dart
+++ lib/models/article.dart
@@ -5,11 +5,9 @@ class Article {
   final String category;
   final String description;
   final String image;
-  bool _isFavorite = false;
-  bool _inCart = false;
 
   Article({required this.id, required this.title, required this.price, required this.category,  required this.description, 
-    required this.image, required bool isFavorite, required bool inCart, })  : _isFavorite = isFavorite, _inCart = inCart;
+    required this.image });
 
   factory Article.fromJson(Map<String, dynamic> jsonData) {
     return Article(
@@ -19,19 +17,6 @@ class Article {
       category: jsonData['category'], 
       description: jsonData['description'], 
       image: jsonData['image'], 
-      isFavorite: false, 
-      inCart: false
     );
-  }
-
-  bool get isFavorite => _isFavorite;
-  bool get inCart => _inCart;
-
-  set isFavorite(bool newValue) {
-    _isFavorite = newValue;
-  }
-
-  set inCart(bool newValue) {
-    _inCart = newValue;
   }
 }
