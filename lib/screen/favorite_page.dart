5e3b3f9efe4fbab1227b0a4b61744505c7b9e526
Djeevs-Jean <jeanyvesbadette@gmail.com>
2023-05-14 16:57:27 -0400

    FavoritePage

--- lib/screen/favorite_page.dart
+++ lib/screen/favorite_page.dart
@@ -3,21 +3,9 @@ import 'package:flutter_ecommerce/storage/storage.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_ecommerce/notifier_chg.dart';
 import 'package:flutter_ecommerce/_widget/_widget_product.dart';
-import 'package:flutter_ecommerce/data_program.dart';
 
-class FavoritePage extends StatefulWidget {
-  static var title;
-
+class FavoritePage extends StatelessWidget {
   const FavoritePage({super.key});
-
-  @override
-  State<FavoritePage> createState() => _FavoritePageState();
-}
-
-class _FavoritePageState extends State<FavoritePage> {
-
-
-  List listFavorites = DataProgram.listFavorites;
 
   @override
   Widget build(BuildContext context) {
@@ -31,26 +19,5 @@ class _FavoritePageState extends State<FavoritePage> {
         return Storage.getFavoriteProdcut(-1);
       },);
     }
-    /* return listFavorites.isEmpty ? const Center(child: Text("Tu n'as pas d'article favoris"),) :
-    ListView.builder(
-      itemCount: listFavorites.length,
-      itemBuilder: (BuildContext context, int index) {
-      return ListTile(
-        leading: Image.network(
-            listFavorites[index].image,
-            width: 50,
-            height: 50,
-          ),
-          title: Text(listFavorites[index].title),
-          subtitle: Text('\$${listFavorites[index].price}'),
-          trailing: IconButton(
-            icon: const Icon(Icons.add_shopping_cart),
-            onPressed: () {
-              // Ajouter le produit au panier ici
-            },
-          ),
-        );
-      },
-    );
- */  }
+  }
 }
