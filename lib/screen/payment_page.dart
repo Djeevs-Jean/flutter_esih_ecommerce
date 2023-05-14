b5a5df76ca4b1387cc6b9444bf5eb43aadb27712
Djeevs-Jean <jeanyvesbadette@gmail.com>
2023-05-14 16:54:09 -0400

    paymentpage

--- lib/screen/payment_page.dart
+++ lib/screen/payment_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class PayementPage extends StatelessWidget {
-  const PayementPage({Key? key});
+  const PayementPage({Key? key}):super(key: key);
 
   @override
   Widget build(BuildContext context) {
