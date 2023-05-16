6e15e71661019ae52e935d0a6e04fa467d85a934
Djeevs-Jean <jeanyvesbadette@gmail.com>
2023-05-15 23:26:19 -0400

    pages

--- lib/pages/product_detail.dart
+++ lib/pages/product_detail.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/utils.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_ecommerce/storage/storage.dart';
-import 'package:flutter_ecommerce/models/article.dart';
+import 'package:flutter_ecommerce/_models/article.dart';
 
 class ProductDetail extends StatefulWidget {
   final Article article;
--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_ecommerce/models/article.dart';
+import 'package:flutter_ecommerce/_models/article.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
