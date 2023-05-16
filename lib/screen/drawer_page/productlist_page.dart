9bbb3a9ed1cae7ab58341b22136fd121a15099ea
Djeevs-Jean <jeanyvesbadette@gmail.com>
2023-05-15 20:42:29 -0400

    add peye

--- lib/screen/drawer_page/productlist_page.dart
+++ lib/screen/drawer_page/productlist_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/utils.dart';
+import 'package:flutter_ecommerce/screen/payment_page.dart';
 import 'package:flutter_ecommerce/service/api_service.dart';
 import 'package:flutter_ecommerce/_widget/_widget_product.dart';
 
@@ -14,7 +16,13 @@ class _ProductListPageState extends State<ProductListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Product List")),
+      appBar: AppBar(title: const Text("Product List"),
+      actions: [
+          TextButton(onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (_) => const PayementPage()));
+          }, child: const Text("PEYE", style: UtilsTheme.titleHead, ))
+        ],
+      ),
       body: const SingleChildScrollView(
         child: ProductListWidget(getProducts: APIService.getProducts),
       ) 
